21b1cbba6e5a469abd7928c1a41c2a41e0ebc31d
Linaa <anhelina.pryadko@gmail.com>
2026-02-19 15:01:22 +0000

    prepare project for the last part

--- src/components/Note.jsx
+++ src/components/Note.jsx
@@ -1,8 +1,9 @@
 import React from "react";
+import DeleteIcon from "@mui/icons-material/Delete";
 
 function Note(props) {
   return (
-    <div className="note">
+    <div className="create-note">
       <h1>{props.title}</h1>
       <p>{props.content}</p>
       <button
