a3685dd856531dd2c5cf8d95be94ecd0ce036a9b
Anhelina Priadko <anhelina.pryadko@gmail.com>
2026-02-28 21:27:27 +0000

    initial commit

---
+++ src/dkeeper_frontend/src/components/App.jsx
@@ -0,0 +1,67 @@
+import React, { useState } from "react";
+import Header from "./Header";
+import Footer from "./Footer";
+import Note from "./Note";
+import CreateArea from "./CreateArea";
+
+function App() {
+  const [note, setNote] = useState({
+    title: "",
+    content: "",
+  });
+  const [notesList, setNotesList] = useState([]);
+
+  function addNote(event) {
+    setNotesList((prevState) => {
+      return [...prevState, note];
+    });
+    setNote({
+      title: "",
+      content: "",
+    });
+
+    event.preventDefault();
+  }
+
+  function deleteNote(id) {
+    setNotesList((prevState) => {
+      return prevState.filter((value, index) => {
+        return index !== id;
+      });
+    });
+  }
+
+  function handleInputEvent(event) {
+    const { name, value } = event.target;
+    setNote((prevState) => {
+      return {
+        ...prevState,
+        [name]: value,
+      };
+    });
+  }
+  return (
+    <div>
+      <Header />
+      <CreateArea
+        onAdd={addNote}
+        onInputChange={handleInputEvent}
+        noteState={note}
+      />
+      {notesList.map((note, index) => {
+        return (
+          <Note
+            key={index}
+            id={index}
+            title={note.title}
+            content={note.content}
+            onDelete={deleteNote}
+          />
+        );
+      })}
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
